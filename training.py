a83068280442c2eaf9a7e0c8a11cdfa55f9540e1
Gabriel Melo <gmelo89@hotmail.com>
2024-04-09 01:52:15 -0400

    training script update

--- training.py
+++ training.py
@@ -23,7 +23,7 @@ def train_model(X_train, X_test, y_train, y_test):
 
     model.compile(loss="mean_squared_error", optimizer="adam")
     model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=150, batch_size=16)
-    model.save("object_detection.h5")
+    model.save("plate_detection.h5")
     print("Model trained and saved")
 
 
